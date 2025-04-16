9793d01966d019b09d626b93e66e8d24df1789d1
lseixas <lseixas.iorio@gmail.com>
2025-04-16 16:01:31 -0300

    trying to add a program folder

--- iac/IacStack.cs
+++ iac/IacStack.cs
@@ -1,14 +1,1 @@
-﻿namespace iac;
-
-public class IacStack
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("CDK project initialization successful!");
-            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
-            Console.WriteLine($".NET version: {Environment.Version}");
-        }
-    }
-}
+﻿
---
+++ iac/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.CDK;
+
+namespace Iac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            var awsRegion = Environment.GetEnvironmentVariable("AWS_REGION");
+            var awsAccountId = Environment.GetEnvironmentVariable("AWS_ACCOUNT_ID");
+            var stackName = Environment.GetEnvironmentVariable("STACK_NAME") ?? "LockLairStackAuthdev";
+            var githubRef = Environment.GetEnvironmentVariable("GITHUB_REF_NAME") ?? "dev";
+
+            string stage = githubRef switch
+            {
+                "prod" => "PROD",
+                "homolog" => "HOMOLOG",
+                "dev" => "DEV",
+                _ => "TEST"
+            };
+
+            var tags = new Dictionary<string, string>
+            {
+                { "project", "Reservation Courts and Schedule MSS" },
+                { "stage", stage },
+                { "stack", "BACK" },
+                { "owner", "DevCommunity" }
+            };
+
+            app.Synth();
+        }
+    }
+}
