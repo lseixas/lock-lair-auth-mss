799c32c79ca62525b13865100b1e03a17a63319d
lseixas <lseixas.iorio@gmail.com>
2025-04-16 11:44:13 -0300

    attempting test

---
+++ src/modules/CI.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultNamespace;
+
+public class CI
+{
+    public string Run()
+    {
+        return "CI rodando!";
+    }
+}
---
+++ test/modules/CI_test.cs
@@ -0,0 +1,16 @@
+﻿using DefaultNamespace;
+using Xunit;
+
+namespace test;
+
+public class CI_test
+{
+    [Fact]
+    public void run_test()
+    {
+        var ciTest = new CI();
+        var result = ciTest.Run();
+
+        Assert.Equal("CI rodando!", result);    
+    }
+}
