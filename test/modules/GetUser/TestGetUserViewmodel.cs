286807fa17ef49722df2368f77c7fb48d4ff7523
lseixas <lseixas.iorio@gmail.com>
2025-04-30 13:55:19 -0300

    testing viewmodel component

---
+++ test/modules/GetUser/TestGetUserViewmodel.cs
@@ -0,0 +1,24 @@
+﻿using GetUser;
+using shared.domain.entities;
+
+namespace test.modules.GetUser;
+
+public class TestGetUserViewmodel
+{
+    [Fact]
+    public void TestToDict()
+    {
+        var user = new User("John Doe");
+        var viewModel = new GetUserViewmodel(user);
+        
+        var dict = viewModel.ToDict();
+
+        var expected = new Dictionary<String, String>()
+        {
+            { "name", user.getName() }
+        };
+        
+        Assert.Equal("John Doe", dict["name"]);
+        Assert.Equal(expected, dict);
+    }
+}
